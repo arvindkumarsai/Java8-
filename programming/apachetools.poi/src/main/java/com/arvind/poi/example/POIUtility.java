a355bd48131269c4aa55c06f3ca6fcda3b5d81ff
arvind_chimta <arvind_chimta@epam.com>
2019-05-15 12:36:08 +0530

    Added java logic

--- programming/apachetools.poi/src/main/java/com/arvind/poi/example/POIUtility.java
+++ programming/apachetools.poi/src/main/java/com/arvind/poi/example/POIUtility.java
@@ -1,25 +1,26 @@
 package com.arvind.poi.example;
 
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.util.SystemOutLogger;
 
-import java.io.File;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
+import java.io.File;
+import java.util.*;
 
 public class POIUtility {
     public static final int TYPE_ROW = 5;
     public static final int TYPE_CELL= 3;
-    public static Map<String,Integer> stateMap = new HashMap<>();
+    public static Map<String,Integer> stateMap = new LinkedHashMap<>();
 
     public static void main(String[] args) throws Exception {
 
-        Sheet sheet0 =  ExcelUtils.getWorkSheetFromFileParams(new File("D://test//Benefits_PBS.xlsx"), 0);
+        Sheet sheet0 =  ExcelUtils.getWorkSheetFromFileParams(new File("D://test//PBS_Benefit_many.xlsx"), 0);
         Row row6 = sheet0.getRow(TYPE_ROW);
         resolveType(row6.getCell(TYPE_CELL).toString(), sheet0);
 
@@ -27,65 +28,98 @@ public class POIUtility {
     }
 
     private static void resolveType(String s, Sheet sheet0) {
-        System.out.println("resolveType");
+
+        int numOfRows = sheet0.getLastRowNum();
+        extractStateMap(sheet0, TYPE_ROW-1);
         if("$Benefit".equalsIgnoreCase(s)) {
-            readBenefitValues(sheet0);
+            readBenefitValues(sheet0, numOfRows);
 
         } /*else {
 
         }*/
     }
 
-    private static void readBenefitValues(Sheet sheet0) {
+    private static void readBenefitValues(Sheet sheet0, int numOfRows) {
         System.out.println("Read Benefit Sheet");
-        int numOfRows = sheet0.getLastRowNum();
-        extractStateMap(sheet0, numOfRows);
+        List<PBSData> pbsDataList = new ArrayList<>();
+        String month,rptType, pbsCode=month=rptType =pbsCode= "";
 
-        for(int rowNum = 4; rowNum < numOfRows ; rowNum++ ) {
+        for(int rowNum = 6; rowNum < numOfRows ; rowNum++ ) {
 
                 Row row = sheet0.getRow(rowNum);
                 int numOfCellPerRow = row.getLastCellNum();
 
-                if(row.getCell(0).toString() != null && ! row.getCell(0).toString().isEmpty()) {
+              for(int j =3 ; j < numOfCellPerRow - 1 ; j++) {
 
-                    stateMap.values().forEach(v -> {
-                        if(row.getCell(1).toString().equals("PBS"))
-                        System.out.println(row.getCell(0)+ ":"  + row.getCell(1) + "value :" + row.getCell(v));
+                    if(row.getCell(1).toString().equalsIgnoreCase("RPBS")) {
+
+                        break;
+                    }
+
+                    if(! row.getCell(0).toString().isEmpty()) {
+                        pbsCode = row.getCell(0).toString();
+                    }
+                    if(!row.getCell(1).toString().isEmpty()) {
+                        if(row.getCell(1).toString().equalsIgnoreCase("PBS")) {
+                            rptType = row.getCell(1).toString();
+                        } else {
+                            break;
+                        }
+
+                    }
 
 
-
-                    });
-
-
+                    if(!row.getCell(2).toString().equalsIgnoreCase("Total")) {
+                        month = row.getCell(2).toString();
+                    } else {
+                        break;
+                    }
+                    if(row.getCell(j) != null &&  !row.getCell(j).toString().isEmpty()) {
+                        generatePBSObject(pbsCode, rptType, month, row, pbsDataList);
+                        break;
+                    }
                 }
-
 
 
             }
 
-
+        pbsDataList.forEach( System.out::println );
         }
 
+    private static void generatePBSObject(String finalPbsCode, String rptType, String month, Row row, List<PBSData> pbsDataList) {
 
-    private static void extractStateMap(Sheet sheet0, int numOfRows) {
-        Iterator<Row> rowIterator = sheet0.rowIterator();
+        stateMap.forEach((k, v)-> {
+            PBSData pbsData = new PBSData();
+            pbsData.setRPTType(rptType);
+            pbsData.setPBS_CODE(finalPbsCode);
+            pbsData.setState(k);
+            pbsData.setValue((int)row.getCell(v).getNumericCellValue());
+            pbsData.setMonth(month);
+            pbsDataList.add(pbsData);
+        });
+    }
 
-        //   for(int rowNum = 4; rowNum < numOfRows ; rowNum++ ) {
-        Row row = sheet0.getRow(4);
+
+    private static void extractStateMap(Sheet sheet0, int state_row) {
+        Row row = sheet0.getRow(state_row);
         int numOfCellPerRow = row.getLastCellNum();
+        Map<String,Integer> unSortedStateMap = new HashMap<String,Integer>();
         for(int cellNum=0;cellNum<numOfCellPerRow;cellNum++){
            String cellValue = row.getCell(cellNum).toString();
-            System.out.println(cellValue + ":" + cellNum);
-
 
            if( null != cellValue  && !cellValue.isEmpty()) {
-               stateMap.put(cellValue, cellNum);
+               unSortedStateMap.put(cellValue, cellNum);
            }
        }
 
-        // }
+       stateMap = unSortedStateMap
+                .entrySet()
+                .stream()
+                .sorted(comparingByValue())
+                .collect(
+                        toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                LinkedHashMap::new));
+       stateMap.forEach((k,v)-> System.out.println("   Map key " + k +"  " +v));
 
-
-        stateMap.forEach((k,v)-> System.out.println("Map key" + k +":" +v));
     }
 }
